0442de81fed887d0c2a50136d059b4b52ee29dcd
zonin <business.kariukimartin@gmail.com>
2023-04-29 16:32:05 +0300

    add pipelined request

--- client.cpp
+++ client.cpp
@@ -53,34 +53,45 @@ static int32_t write_all(int fd, const char *buf, size_t n)
     return 0;
 }
 
-const size_t k_max_msg = 4096; 
+const size_t k_max_msg = 4096;
+//  the `query` function splited into `send_req` and `read_res`.
 
-static int32_t query(int fd, const char *text){
+static int32_t send_req(int fd, const char *text)
+{
     uint32_t len = (uint32_t)strlen(text);
-    if(len > k_max_msg) return -1;
+    if (len > k_max_msg)
+    {
+        return -1;
+    }
 
     char wbuf[4 + k_max_msg];
-    memcpy(wbuf, &len, 4); // assume little endian
+    memcpy(wbuf, &len, 4);
     memcpy(&wbuf[4], text, len);
-    if(int32_t err = write_all(fd, wbuf, 4+len)) return err;
+    return write_all(fd, wbuf, 4 + len);
+}
 
+static int32_t read_res(int fd)
+{
     // 4 bytes header
-
     char rbuf[4 + k_max_msg + 1];
-    errno =0;
+    errno = 0;
     int32_t err = read_full(fd, rbuf, 4);
-    if(err){
-        if(errno == 0){
+    if (err)
+    {
+        if (errno == 0)
+        {
             msg("EOF");
-        } else {
+        }
+        else
+        {
             msg("read() error");
         }
         return err;
     }
 
+    uint32_t len = 0;
     memcpy(&len, rbuf, 4); // assume little endian
-
-    if(len > k_max_msg)
+    if (len > k_max_msg)
     {
         msg("too long");
         return -1;
@@ -88,7 +99,8 @@ static int32_t query(int fd, const char *text){
 
     // reply body
     err = read_full(fd, &rbuf[4], len);
-    if(err){
+    if (err)
+    {
         msg("read() error");
         return err;
     }
@@ -97,12 +109,12 @@ static int32_t query(int fd, const char *text){
     rbuf[4 + len] = '\0';
     printf("server says: %s\n", &rbuf[4]);
     return 0;
-
 }
 
-int main(){
+int main()
+{
     int fd = socket(AF_INET, SOCK_STREAM, 0);
-    if(fd < 0)
+    if (fd < 0)
     {
         die("socket()");
     }
@@ -114,32 +126,30 @@ int main(){
 
     int rv = connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
 
-    if(rv)
+    if (rv)
     {
         die("connect");
     }
-    
 
-    // multiple requests
-
-    int32_t err = query(fd, "hello1");
-    if(err){
-        goto L_DONE;
+    // multiple piplined requests
+    const char *query_list[3] = {"hello", "hello2", "hello3"};
+    for (size_t i = 0; i < 3; ++i)
+    {
+        int err = send_req(fd, query_list[i]);
+        if (err)
+        {
+            goto L_DONE;
+        }
     }
 
-    err = query(fd, "hello2");
-    if (err)
+    for (size_t i = 0; i < 3; ++i)
     {
-        goto L_DONE;
-    }
-    err = query(fd, "hello2");
-    if (err)
-    {
-        goto L_DONE;
+        int32_t err = read_res(fd);
+        if (err)
+            goto L_DONE;
     }
 
 L_DONE:
     close(fd);
     return 0;
-
 }
