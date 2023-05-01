fbdc6132f78489634d4623823e9117712dc553d6
zonin <business.kariukimartin@gmail.com>
2023-05-01 05:25:18 +0300

    send request

--- 1.01/client.cpp
+++ 1.01/client.cpp
@@ -8,6 +8,11 @@
 #include <sys/socket.h>
 #include <netinet/ip.h>
 #include <assert.h>
+#include <vector>
+#include <string>
+
+using std::vector;
+using std::string;
 
 static void msg(const char *msg)
 {
@@ -56,17 +61,28 @@ static int32_t write_all(int fd, const char *buf, size_t n)
 const size_t k_max_msg = 4096;
 //  the `query` function splited into `send_req` and `read_res`.
 
-static int32_t send_req(int fd, const char *text)
+static int32_t send_req(int fd, const vector<string> &cmd)
 {
-    uint32_t len = (uint32_t)strlen(text);
+    uint32_t len = 4;
+    for (const string &s : cmd){
+        len += 4 + s.size();
+    }
     if (len > k_max_msg)
     {
         return -1;
     }
 
     char wbuf[4 + k_max_msg];
-    memcpy(wbuf, &len, 4);
-    memcpy(&wbuf[4], text, len);
+    memcpy(&wbuf[0], &len, 4); // assume little endian
+    uint32_t n = cmd.size();
+    memcpy(&wbuf[4], &n, 4);
+    size_t cur = 8;
+    for(const string &s : cmd){
+        uint32_t p = (uint32_t)s.size();
+        memcpy(&wbuf[cur], &p, 4);
+        memcpy(&wbuf[cur + 4], s.data(), s.size());
+    }
+
     return write_all(fd, wbuf, 4 + len);
 }
 
@@ -111,7 +127,7 @@ static int32_t read_res(int fd)
     return 0;
 }
 
-int main()
+int main(int argc, char **argv)
 {
     int fd = socket(AF_INET, SOCK_STREAM, 0);
     if (fd < 0)
@@ -132,22 +148,12 @@ int main()
     }
 
     // multiple piplined requests
-    const char *query_list[3] = {"hello", "hello2", "hello3"};
-    for (size_t i = 0; i < 3; ++i)
-    {
-        int err = send_req(fd, query_list[i]);
-        if (err)
-        {
-            goto L_DONE;
-        }
+    vector<string> cmd;
+    for(int i = 1; i < argc; ++i){
+        cmd.push_back(argv[i]);
     }
 
-    for (size_t i = 0; i < 3; ++i)
-    {
-        int32_t err = read_res(fd);
-        if (err)
-            goto L_DONE;
-    }
+    int32_t err = send_req(fd, cmd);
 
 L_DONE:
     close(fd);
