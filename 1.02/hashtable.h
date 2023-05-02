b4801c38ffd306cbd69c40c237d973a22cd10bfc
zonin <business.kariukimartin@gmail.com>
2023-05-02 13:42:23 +0300

    format code

--- 1.02/hashtable.h
+++ 1.02/hashtable.h
@@ -3,13 +3,15 @@
 
 // hashtable node, should be embedded into the payload
 
-struct HNode {
+struct HNode
+{
     HNode *next = NULL;
     uint64_t hcode = 0;
 };
 
 // a simple fixed-seized hashtable
-struct HTab {
+struct HTab
+{
     HNode **tab = NULL;
     size_t mask = 0;
     size_t size = 0;
@@ -17,12 +19,13 @@ struct HTab {
 
 // interface
 
-struct HMap{
+struct HMap
+{
     HTab ht1;
     HTab ht2;
     size_t resizing_pos = 0;
 };
 
-HNode *hm_lookup(HMap *hmap, HNode *key, bool(*cmp)(HNode *, HNode *));
+HNode *hm_lookup(HMap *hmap, HNode *key, bool (*cmp)(HNode *, HNode *));
 void hm_insert(HMap *hmap, HNode *node);
-HNode *hm_pop(HMap *hmap, HNode *key, bool(*cmp)(HNode *, HNode *));
+HNode *hm_pop(HMap *hmap, HNode *key, bool (*cmp)(HNode *, HNode *));
